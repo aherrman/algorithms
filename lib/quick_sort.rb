ecd9de90bb0ee01cbdf9b6164f5a03572da40df6
Andy Herrman <aherrman@gmail.com>
2010-03-14 11:41:54 -0400

    quicksort implementation

--- lib/quick_sort.rb
+++ lib/quick_sort.rb
@@ -1,8 +1,22 @@
 module QuickSort
   def sort(toSort)
+    return toSort if toSort.size <= 1
+    pivot = rand toSort.size
+    pivotValue = toSort[pivot]
 
+    left = []
+    right = []
+
+    toSort.each_with_index do |value, index|
+      next if index == pivot
+
+      if (value <=> pivotValue) <= 0
+        left.push value
+      else
+        right.push value
+      end
+    end
+
+    sort(left).concat([pivotValue]).concat(sort(right))
   end
-
-private
-  
 end
