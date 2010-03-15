01784718c67cea720e343391691b12935cbc28c9
Andy Herrman <aherrman@gmail.com>
2010-03-14 20:59:24 -0400

    Added in place quicksort

---
+++ lib/in_place_quick_sort.rb
@@ -0,0 +1,37 @@
+module InPlaceQuickSort
+  def sort(toSort)
+    sort_in_place(toSort, 0, toSort.size - 1)
+  end
+
+private
+  def sort_in_place(toSort, left, right)
+    return toSort unless right - left > 1
+    pivot = left + rand(right - left)
+    pivotValue = toSort[pivot]
+
+    swap(toSort, pivot, right)
+
+    currIndex = left
+    (left...right).each do |i|
+      value = toSort[i]
+      if (value <=> pivotValue) < 0
+        swap(toSort, currIndex, i)
+        currIndex = currIndex + 1
+      end
+    end
+
+    swap(toSort, currIndex, right)
+
+
+    sort_in_place(toSort, left, currIndex - 1)
+    sort_in_place(toSort, currIndex + 1, right)
+
+    toSort
+  end
+
+  def swap(array, index1, index2)
+    tmp = array[index1]
+    array[index1] = array[index2]
+    array[index2] = tmp
+  end
+end
---
+++ test/in_place_quick_sort_test.rb
@@ -0,0 +1,29 @@
+# Unit tests for MergeSort
+
+$:.unshift File.join(File.dirname(__FILE__),'..','lib')
+
+require 'test/unit'
+require 'in_place_quick_sort'
+
+class IPQSTest
+  extend InPlaceQuickSort
+end
+
+class InPlaceQuickSortTest < Test::Unit::TestCase
+  def test_sort
+    unsorted = [6, 3, 8, 4, 5, 0, 2, 9, 7, 1]
+
+    sorted = IPQSTest.sort unsorted
+    expected = unsorted.sort
+
+    assert_equal expected, sorted
+  end
+
+  def test_sort_changes_input
+    unsorted = [6, 3, 8, 4, 5, 0, 2, 9, 7, 1]
+
+    sorted = IPQSTest.sort unsorted
+
+    assert sorted.equal? unsorted
+  end
+end
